542db0342ac954fb04f69c9bda10a8392b52a0c3
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-10-25 11:19:28 -0500

    Roles appear to be set up

---
+++ Trash_Collector/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trash_Collector.Models;
+
+namespace Trash_Collector.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged In";
+            }
+            return View();
+        }        
+    }
+}
